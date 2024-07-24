a414bbf741a046c3169eae103530b35875038025
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-07-24 15:31:35 +0200

    add verify batches check

--- test/run-e2e-seq_sender.sh
+++ test/run-e2e-seq_sender.sh
@@ -30,3 +30,5 @@ $BASE_FOLDER/scripts/kurtosis_prepare_params_yml.sh "$KURTOSIS_FOLDER" "elderber
 kurtosis clean --all
 kurtosis run --enclave cdk-v1 --args-file $DEST_KURTOSIS_PARAMS_YML --image-download always $KURTOSIS_FOLDER
 [ $? -ne 0 ] && echo "Error running kurtosis" && exit 1
+echo "Waiting 10 minutes to get some verified batch...."
+$KURTOSIS_FOLDER/.github/actions/monitor-cdk-verified-batches/batch_verification_monitor.sh 1 600
