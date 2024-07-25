983da81989cc20370a1966ff20060d6c9f06f279
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-07-25 12:37:12 +0200

    disabled datacommitte test because PUSH0 is not supported by geth simulated

--- dataavailability/datacommittee/datacommittee_test.go
+++ dataavailability/datacommittee/datacommittee_test.go
@@ -16,6 +16,7 @@ import (
 )
 
 func TestUpdateDataCommitteeEvent(t *testing.T) {
+	t.Skip("This test is not working because the simulated backedn doesnt accept PUSH0, check: https://github.com/ethereum/go-ethereum/issues/28144#issuecomment-2247124776")
 	// Set up testing environment
 	dac, ethBackend, auth, da := newTestingEnv(t)
 
