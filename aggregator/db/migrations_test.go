7e796ea25cdcd96a87355351d9241bde78643b7a
Victor Castell <0x@vcastellm.xyz>
2024-07-26 14:28:27 +0000

    Add test on migrations

---
+++ aggregator/db/migrations_test.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"testing"
+
+	migrate "github.com/rubenv/sql-migrate"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_checkMigrations(t *testing.T) {
+	embedMigration := embedMigrations[AggregatorMigrationName]
+	migrationSource := &migrate.EmbedFileSystemMigrationSource{FileSystem: embedMigration}
+
+	_, err := migrationSource.FileSystem.ReadFile("migrations/aggregator/0001.sql")
+	assert.NoError(t, err)
+}
