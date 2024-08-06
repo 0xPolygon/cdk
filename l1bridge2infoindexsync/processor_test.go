27aa35c82281d5ba44b8cd4f3b8799f135acbe9c
Arnau <arnaubennassar5@gmail.com>
2024-08-06 10:39:25 +0200

    fix duplicated key case

---
+++ l1bridge2infoindexsync/processor_test.go
@@ -0,0 +1,22 @@
+package l1bridge2infoindexsync
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDuplicatedKey(t *testing.T) {
+	dbPath := t.TempDir()
+	p, err := newProcessor(dbPath)
+	require.NoError(t, err)
+	ctx := context.Background()
+	err = p.processUntilBlock(ctx, 5, []bridge2L1InfoRelation{{bridgeIndex: 2, l1InfoTreeIndex: 2}})
+	require.NoError(t, err)
+	err = p.processUntilBlock(ctx, 7, []bridge2L1InfoRelation{{bridgeIndex: 2, l1InfoTreeIndex: 3}})
+	require.NoError(t, err)
+	l1InfoTreeIndex, err := p.getL1InfoTreeIndexByBrdigeIndex(ctx, 2)
+	require.NoError(t, err)
+	require.Equal(t, uint32(2), l1InfoTreeIndex)
+}
