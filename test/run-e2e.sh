e4b5bb84bd25207912357d300e5e9c400a1a55c1
Victor Castell <0x@vcastellm.xyz>
2024-08-13 16:40:04 +0000

    refactor: simplify, trim and remove defaults

--- test/run-e2e.sh
+++ test/run-e2e.sh
@@ -6,13 +6,7 @@ if [ -z $DATA_AVAILABILITY_MODE ]; then
     echo "Missing DATA_AVAILABILITY_MODE: ['rollup', 'cdk-validium']"
     exit 1
 fi
-KURTOSIS_VERSION=jesteban/cdk-seq_sender
 BASE_FOLDER=$(dirname $0)
-KURTOSIS_FOLDER=$($BASE_FOLDER/scripts/get_kurtosis_clone_folder.sh $KURTOSIS_VERSION)
-[ $? -ne 0 ] && echo "Error getting kurtosis folder" && exit 1
-
-$BASE_FOLDER/scripts/clone_kurtosis.sh $KURTOSIS_VERSION "$KURTOSIS_FOLDER"
-[ $? -ne 0 ] && echo "Error cloning kurtosis " && exit 1
 
 docker images -q cdk:latest > /dev/null
 if [ $? -ne 0 ] ; then
