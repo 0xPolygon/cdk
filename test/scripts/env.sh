000b7661a948fd254fad620b2cedc1b46f749f53
Victor Castell <0x@vcastellm.xyz>
2024-08-14 09:29:37 +0000

    ci: opinion

--- test/scripts/env.sh
+++ test/scripts/env.sh
@@ -4,4 +4,4 @@ ENCLAVE=cdk-v1
 TMP_CDK_FOLDER=tmp/cdk
 DEST_KURTOSIS_PARAMS_YML=../$TMP_CDK_FOLDER/e2e-params.yml
 KURTOSIS_VERSION=develop
-KURTOSIS_FOLDER=kurtosis-cdk
+KURTOSIS_FOLDER=../kurtosis-cdk
