9878f1477e0d4afcb6a424e1dec8a607b61aae50
Toni Ramírez <58293609+ToniRamirezM@users.noreply.github.com>
2024-08-19 14:28:16 +0200

    feat: Update seq sender and aggregator (#38)
    * feat: bring latest changes

--- sequencesender/txbuilder/banana_zkevm.go
+++ sequencesender/txbuilder/banana_zkevm.go
@@ -64,7 +64,7 @@ func (t *TxBuilderBananaZKEVM) BuildSequenceBatchesTx(ctx context.Context, seque
 	// Build sequence data
 	tx, err := t.sequenceBatchesRollup(newopts, ethseq)
 	if err != nil {
-		log.Errorf("[SeqSender] error estimating new sequenceBatches to add to ethtxmanager: ", err)
+		log.Errorf("error estimating new sequenceBatches to add to ethtxmanager: ", err)
 		return nil, err
 	}
 	return tx, nil
@@ -91,7 +91,6 @@ func (t *TxBuilderBananaZKEVM) sequenceBatchesRollup(opts bind.TransactOpts, seq
 		log.Debugf("Batches to send: %+v", batches)
 		log.Debug("l2CoinBase: ", sequence.L2Coinbase)
 		log.Debug("Sequencer address: ", opts.From)
-
 	}
 
 	return tx, err
