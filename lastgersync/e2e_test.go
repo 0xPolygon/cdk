c6eac19f1429cd0cc6b4ef6db79fbcb16526aae9
Toni Ramírez <58293609+ToniRamirezM@users.noreply.github.com>
2024-08-21 16:43:20 +0200

    fix: required fixes to work with Fork ID 12 (#50)
    * fix: fork id 12
    
    * fix: increase sleep value on e2e test

--- lastgersync/e2e_test.go
+++ lastgersync/e2e_test.go
@@ -59,7 +59,7 @@ func TestE2E(t *testing.T) {
 				syncerUpToDate = true
 				break
 			}
-			time.Sleep(time.Millisecond * 10)
+			time.Sleep(time.Millisecond * 100)
 			errMsg = fmt.Sprintf("last block from client: %d, last block from syncer: %d", lb, lpb)
 		}
 		require.True(t, syncerUpToDate, errMsg)
