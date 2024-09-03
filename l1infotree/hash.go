9bcad3b5c31702076db4ff850581ab04b71c1a00
Rachit Sonthalia <rachitsonthalia02@gmail.com>
2024-09-03 17:25:40 +0530

    l1infotree lint fix

--- l1infotree/hash.go
+++ l1infotree/hash.go
@@ -13,9 +13,10 @@ func Hash(data ...[32]byte) [32]byte {
 	var res [32]byte
 	hash := sha3.NewLegacyKeccak256()
 	for _, d := range data {
-		hash.Write(d[:]) //nolint:errcheck,gosec
+		hash.Write(d[:])
 	}
 	copy(res[:], hash.Sum(nil))
+
 	return res
 }
 
@@ -23,11 +24,13 @@ func generateZeroHashes(height uint8) [][32]byte {
 	var zeroHashes = [][32]byte{
 		common.Hash{},
 	}
-	// This generates a leaf = HashZero in position 0. In the rest of the positions that are equivalent to the ascending levels,
-	// we set the hashes of the nodes. So all nodes from level i=5 will have the same value and same children nodes.
+	// This generates a leaf = HashZero in position 0. In the rest of the positions that
+	// are equivalent to the ascending levels, we set the hashes of the nodes.
+	// So all nodes from level i=5 will have the same value and same children nodes.
 	for i := 1; i <= int(height); i++ {
 		zeroHashes = append(zeroHashes, Hash(zeroHashes[i-1], zeroHashes[i-1]))
 	}
+
 	return zeroHashes
 }
 
@@ -37,5 +40,6 @@ func HashLeafData(ger, prevBlockHash common.Hash, minTimestamp uint64) [32]byte 
 	t := make([]byte, 8) //nolint:gomnd
 	binary.BigEndian.PutUint64(t, minTimestamp)
 	copy(res[:], keccak256.Hash(ger.Bytes(), prevBlockHash.Bytes(), t))
+
 	return res
 }
