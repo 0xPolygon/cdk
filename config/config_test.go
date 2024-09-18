32ef9938960b65c1ff528ec21bf865691963139e
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-09-18 13:03:32 +0200

    feat: add ut

--- config/config_test.go
+++ config/config_test.go
@@ -7,11 +7,41 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestXxx(t *testing.T) {
+func TestLoadDeafaultConfig(t *testing.T) {
 	tmpFile, err := os.CreateTemp("", "ut_config")
 	require.NoError(t, err)
 	defer os.Remove(tmpFile.Name())
 	tmpFile.Write([]byte(DefaultValues))
+	cfg, err := LoadFile(tmpFile.Name())
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+}
+
+const configWithUnexpectedFields = `
+[UnknownField]
+Field = "value"
+`
+
+func TestLoadConfigWithUnexpectedFields(t *testing.T) {
+	tmpFile, err := os.CreateTemp("", "ut_config")
+	require.NoError(t, err)
+	defer os.Remove(tmpFile.Name())
+	tmpFile.Write([]byte(configWithUnexpectedFields))
+	cfg, err := LoadFile(tmpFile.Name())
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+}
+
+const configWithForbiddenFields = `
+[aggregator.synchronizer.db]
+name = "value"
+`
+
+func TestLoadConfigWithForbiddenFields(t *testing.T) {
+	tmpFile, err := os.CreateTemp("", "ut_config")
+	require.NoError(t, err)
+	defer os.Remove(tmpFile.Name())
+	tmpFile.Write([]byte(configWithForbiddenFields))
 	cfg, err := LoadFile(tmpFile.Name())
 	require.NoError(t, err)
 	require.NotNil(t, cfg)
