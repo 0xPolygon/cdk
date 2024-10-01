f0ea6a586f94628201e87e7775e2a193885c248d
Victor Castell <0x@vcastellm.xyz>
2024-10-01 13:38:24 +0000

    refactor: simplify running Kurtosis combinations
    Using a set of pre-defined combinations of components,
    we're going to test the versions we're interested in.

--- test/run-e2e.sh
+++ test/run-e2e.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 source $(dirname $0)/scripts/env.sh
-FORK=elderberry
-DATA_AVAILABILITY_MODE=$1
+
+FORK=$1
+if [ -z $FORK ]; then
+    echo "Missing FORK: ['fork9', 'fork12']"
+    exit 1
+fi
+
+DATA_AVAILABILITY_MODE=$2
 if [ -z $DATA_AVAILABILITY_MODE ]; then
     echo "Missing DATA_AVAILABILITY_MODE: ['rollup', 'cdk-validium']"
     exit 1
 fi
-BASE_FOLDER=$(dirname $0)
 
+BASE_FOLDER=$(dirname $0)
 docker images -q cdk:latest > /dev/null
 if [ $? -ne 0 ] ; then
     echo "Building cdk:latest"
@@ -18,10 +24,7 @@ else
     echo "docker cdk:latest already exists"
 fi
 
-$BASE_FOLDER/scripts/kurtosis_prepare_params_yml.sh "$KURTOSIS_FOLDER" $DATA_AVAILABILITY_MODE
-[ $? -ne 0 ] && echo "Error preparing params.yml" && exit 1
-
 kurtosis clean --all
 echo "Override cdk config file"
 cp $BASE_FOLDER/config/kurtosis-cdk-node-config.toml.template $KURTOSIS_FOLDER/templates/trusted-node/cdk-node-config.toml
-kurtosis run --enclave cdk-v1 --args-file $DEST_KURTOSIS_PARAMS_YML --image-download always $KURTOSIS_FOLDER
+kurtosis run --enclave cdk-v1 --args-file "combinations/$FORK-$DATA_AVAILABILITY_MODE.yml" --image-download always $KURTOSIS_FOLDER
