72ed0257900d57ecd2392da4d97504004b8ccddd
Victor Castell <victor@polygon.technology>
2024-10-10 14:59:46 +0200

    feat: add versions command (#120)
    * refactor: not all commands need the same params
    * feat: versions command

--- crates/cdk/src/cli.rs
+++ crates/cdk/src/cli.rs
@@ -5,25 +5,8 @@ use clap::{Parser, Subcommand, ValueHint};
 
 /// Command line interface.
 #[derive(Parser)]
+#[command(author, version, about, long_about = None)]
 pub(crate) struct Cli {
-    /// The path to the configuration file.
-    #[arg(
-        long,
-        short,
-        value_hint = ValueHint::FilePath,
-        env = "CDK_CONFIG_PATH"
-    )]
-    pub(crate) config: PathBuf,
-
-    /// The path to a chain specification file.
-    #[arg(
-        long,
-        short = 'g',
-        value_hint = ValueHint::FilePath,
-        env = "CDK_GENESIS_PATH"
-    )]
-    pub(crate) chain: PathBuf,
-
     #[command(subcommand)]
     pub(crate) cmd: Commands,
 }
@@ -31,6 +14,15 @@ pub(crate) struct Cli {
 #[derive(Subcommand)]
 pub(crate) enum Commands {
     Node {
+        /// The path to the configuration file.
+        #[arg(
+            long,
+            short,
+            value_hint = ValueHint::FilePath,
+            env = "CDK_CONFIG_PATH"
+        )]
+        config: PathBuf,
+
         /// Components to run.
         #[arg(
             long,
@@ -40,5 +32,24 @@ pub(crate) enum Commands {
         )]
         components: Option<String>,
     },
-    Erigon,
+    Erigon {
+        /// The path to the configuration file.
+        #[arg(
+            long,
+            short,
+            value_hint = ValueHint::FilePath,
+            env = "CDK_CONFIG_PATH"
+        )]
+        config: PathBuf,
+
+        /// The path to a chain specification file.
+        #[arg(
+            long,
+            short = 'g',
+            value_hint = ValueHint::FilePath,
+            env = "CDK_GENESIS_PATH"
+        )]
+        chain: PathBuf,
+    },
+    Versions,
 }
