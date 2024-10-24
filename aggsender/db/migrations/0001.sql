21680d828f80c61e923a2ce5d340d00c02dc3088
Goran Rojovic <goran.rojovic@ethernal.tech>
2024-10-24 09:27:16 +0200

    fix: identantion and primary key

--- aggsender/db/migrations/0001.sql
+++ aggsender/db/migrations/0001.sql
@@ -3,12 +3,10 @@ DROP TABLE IF EXISTS certificate_info;
 
 -- +migrate Up
 CREATE TABLE certificate_info (
-	height                      INTEGER NOT NULL,
-	certificate_id              VARCHAR NOT NULL,
+    height                      INTEGER NOT NULL,
+    certificate_id              VARCHAR NOT NULL PRIMARY KEY,
     status                      INTEGER NOT NULL,
     new_local_exit_root         VARCHAR NOT NULL,
     from_block                  INTEGER NOT NULL,
-    to_block                    INTEGER NOT NULL,
-
-    PRIMARY KEY (height, certificate_id)
+    to_block                    INTEGER NOT NULL
 );
