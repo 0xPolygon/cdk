138178be11ff0e0cad0f6088f7ad5cf1438d1455
Toni Ramírez <toni@polygon.technology>
2024-11-04 16:00:16 +0100

    fix: typo

--- common/common.go
+++ common/common.go
@@ -83,7 +83,7 @@ func CalculateAccInputHash(
 	}
 
 	v2 = keccak256.Hash(v2)
-	calculateAccInputHash := common.BytesToHash(keccak256.Hash(v1, v2, v3, v4, v5, v6))
+	calculatedAccInputHash := common.BytesToHash(keccak256.Hash(v1, v2, v3, v4, v5, v6))
 
 	logger.Debugf("OldAccInputHash: %v", oldAccInputHash)
 	logger.Debugf("BatchHashData: %v", common.Bytes2Hex(v2))
@@ -91,9 +91,9 @@ func CalculateAccInputHash(
 	logger.Debugf("TimeStampLimit: %v", timestampLimit)
 	logger.Debugf("Sequencer Address: %v", sequencerAddr)
 	logger.Debugf("Forced BlockHashL1: %v", forcedBlockhashL1)
-	logger.Debugf("CalculatedAccInputHash: %v", calculateAccInputHash)
+	logger.Debugf("CalculatedAccInputHash: %v", calculatedAccInputHash)
 
-	return calculateAccInputHash
+	return calculatedAccInputHash
 }
 
 // NewKeyFromKeystore creates a private key from a keystore file
