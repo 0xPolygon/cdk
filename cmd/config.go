2f197808ec59b6e32f33080e4cbafc126367d6f9
Victor Castell <0x@vcastellm.xyz>
2024-11-21 11:58:46 +0000

    remove extra param

--- cmd/config.go
+++ cmd/config.go
@@ -9,24 +9,11 @@ import (
 )
 
 func configCmd(*cli.Context) error {
-	// var files []config.FileData
-	// fileData := make([]config.FileData, 0)
-	// fileData = append(fileData, config.FileData{Name: "default_mandatory_vars", Content: config.DefaultMandatoryVars})
-	// fileData = append(fileData, config.FileData{Name: "default_vars", Content: config.DefaultVars})
-	// fileData = append(fileData, config.FileData{Name: "default_values", Content: config.DefaultValues})
-	// fileData = append(fileData, files...)
 	// String buffer to concatenate all the default config vars
 	defaultConfig := strings.Builder{}
 	defaultConfig.WriteString(config.DefaultMandatoryVars)
 	defaultConfig.WriteString(config.DefaultVars)
 	defaultConfig.WriteString(config.DefaultValues)
-
-	// merger := config.NewConfigRender(fileData, config.EnvVarPrefix)
-
-	// renderedCfg, err := merger.Render()
-	// if err != nil {
-	// 	return err
-	// }
 
 	_, err := os.Stdout.WriteString(defaultConfig.String())
 	if err != nil {
--- cmd/main.go
+++ cmd/main.go
@@ -62,13 +62,6 @@ var (
 		Usage:    "Allow that config-files contains deprecated fields",
 		Required: false,
 	}
-
-	extendedConfigFlag = cli.StringFlag{
-		Name:     config.FlagExtendedConfig,
-		Aliases:  []string{"e"},
-		Usage:    "Save final configuration into to the indicated path (name: cdk-node-config.toml)",
-		Required: false,
-	}
 )
 
 func main() {
@@ -102,7 +95,6 @@ func main() {
 			Aliases: []string{},
 			Usage:   "Output a default configuration file",
 			Action:  configCmd,
-			Flags:   []cli.Flag{&extendedConfigFlag},
 		},
 	}
 
