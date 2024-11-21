76af52b457f37c4ecd93c8e84681e70c76ade044
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-11-21 12:06:28 +0100

    feat: use combination as template

--- test/run-e2e.sh
+++ test/run-e2e.sh
@@ -27,15 +27,9 @@ fi
 kurtosis clean --all
 echo "Override cdk config file"
 cp $BASE_FOLDER/config/kurtosis-cdk-node-config.toml.template $KURTOSIS_FOLDER/templates/trusted-node/cdk-node-config.toml
-kurtosis_config_file="combinations/$FORK-$DATA_AVAILABILITY_MODE.yml"
-if [ $DATA_AVAILABILITY_MODE == "pessimistic" ]; then
-    if [ ! -z $agglayer_prover_sp1_key ]; then
-        local_config_file=$(mktemp --suffix ".yml")
-        cp "$kurtosis_config_file" $local_config_file
-        echo "Setting agglayer_prover_sp1_key"
-        echo "  agglayer_prover_sp1_key: $agglayer_prover_sp1_key" >> $local_config_file
-        kurtosis_config_file=$local_config_file 
-    fi
-fi
-kurtosis run --enclave cdk --args-file "$kurtosis_config_file" --image-download always $KURTOSIS_FOLDER
-[ !-z $local_config_file ] && rm $local_config_file
+KURTOSIS_CONFIG_FILE="combinations/$FORK-$DATA_AVAILABILITY_MODE.yml"
+TEMP_CONFIG_FILE=$(mktemp --suffix ".yml")
+echo "rendering $KURTOSIS_CONFIG_FILE to temp file $TEMP_CONFIG_FILE"
+go run ../scripts/run_template.go $KURTOSIS_CONFIG_FILE > $TEMP_CONFIG_FILE
+kurtosis run --enclave cdk --args-file "$TEMP_CONFIG_FILE" --image-download always $KURTOSIS_FOLDER
+rm $TEMP_CONFIG_FILE
