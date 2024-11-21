0f4276db85fc97840a91b05aa5f451697a46efd6
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-11-21 11:00:23 +0100

    fix: ci

--- test/run-e2e.sh
+++ test/run-e2e.sh
@@ -9,7 +9,7 @@ fi
 
 DATA_AVAILABILITY_MODE=$2
 if [ -z $DATA_AVAILABILITY_MODE ]; then
-    echo "Missing DATA_AVAILABILITY_MODE: ['rollup', 'cdk-validium']"
+    echo "Missing DATA_AVAILABILITY_MODE: ['rollup', 'cdk-validium', 'pessimistic']"
     exit 1
 fi
 
@@ -28,12 +28,15 @@ kurtosis clean --all
 echo "Override cdk config file"
 cp $BASE_FOLDER/config/kurtosis-cdk-node-config.toml.template $KURTOSIS_FOLDER/templates/trusted-node/cdk-node-config.toml
 kurtosis_config_file="combinations/$FORK-$DATA_AVAILABILITY_MODE.yml"
-if [ ! -z $agglayer_prover_sp1_key ]; then
-    local_config_file=$(mktemp)
-    cp "$kurtosis_config_file" $local_config_file
-    echo "Setting agglayer_prover_sp1_key"
-    echo "  agglayer_prover_sp1_key:$agglayer_prover_sp1_key" >> $local_config_file
-    kurtosis_config_file=$local_config_file
+if [ $DATA_AVAILABILITY_MODE == "pessimistic" ]; then
+    if [ ! -z $agglayer_prover_sp1_key ]; then
+        local_config_file=$(mktemp --suffix ".yml")
+        cp "$kurtosis_config_file" $local_config_file
+        echo "Setting agglayer_prover_sp1_key"
+        echo "  agglayer_prover_sp1_key: $agglayer_prover_sp1_key" >> $local_config_file
+        kurtosis_config_file=$local_config_file
+        echo $local_config_file
+    fi
 fi
-kurtosis run --enclave cdk --args-file "$kurtosis_config_file" --image-download always $KURTOSIS_FOLDER
-rm $local_config_file
+#kurtosis run --enclave cdk --args-file "$kurtosis_config_file" --image-download always $KURTOSIS_FOLDER
+#rm $local_config_file
