feec5ad5c4decd27d774120aa8f6ab6ddc6d1baa
Goran Rojovic <goran.rojovic@ethernal.tech>
2025-01-24 09:51:57 +0100

    fix: lint

--- test/helpers/e2e.go
+++ test/helpers/e2e.go
@@ -184,9 +184,9 @@ func L2Setup(t *testing.T) *L2Environment {
 	dbPathReorgL2 := path.Join(t.TempDir(), "ReorgDetectorL2.sqlite")
 	rdL2, err := reorgdetector.New(l2Client.Client(), reorgdetector.Config{
 		DBPath:              dbPathReorgL2,
-		CheckReorgsInterval: cfgTypes.Duration{Duration: time.Millisecond * 100}},
+		CheckReorgsInterval: cfgTypes.Duration{Duration: time.Millisecond * 100}}, //nolint:mnd
 		reorgdetector.L2,
-	) //nolint:mnd
+	)
 	require.NoError(t, err)
 	go rdL2.Start(ctx) //nolint:errcheck
 
