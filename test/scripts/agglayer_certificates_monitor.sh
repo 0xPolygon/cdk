92eb06a9d90012ee52be82a48522119d6c52e180
Stefan Negovanović <stefan@ethernal.tech>
2025-03-14 13:47:53 +0100

    fix: agglayer_certificates_monitor script

--- test/scripts/agglayer_certificates_monitor.sh
+++ test/scripts/agglayer_certificates_monitor.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # This script monitors the agglayer certificates progress of pessimistic proof.
 
-function parse_params(){
+function parse_params() {
     # Check if the required arguments are provided.
     if [ "$#" -lt 3 ]; then
-    echo "Usage: $0 <settle_certificates_target> <timeout> <l2_network_id>"
-    exit 1
+        echo "Usage: $0 <settle_certificates_target> <timeout> <l2_network_id>"
+        exit 1
     fi
 
     # The number of batches to be verified.
@@ -18,7 +18,7 @@ function parse_params(){
     l2_rpc_network_id="$3"
 }
 
-function check_timeout(){
+function check_timeout() {
     local _end_time=$1
     current_time=$(date +%s)
     if ((current_time > _end_time)); then
@@ -27,8 +27,8 @@ function check_timeout(){
     fi
 }
 
-function check_num_certificates(){
-    readonly agglayer_rpc_url="$(kurtosis port print cdk agglayer agglayer)"
+function check_num_certificates() {
+    readonly agglayer_rpc_url="$(kurtosis port print cdk agglayer aglr-readrpc)"
 
     cast_output=$(cast rpc --rpc-url "$agglayer_rpc_url" "interop_getLatestKnownCertificateHeader" "$l2_rpc_network_id" 2>&1)
 
@@ -51,17 +51,17 @@ function check_num_certificates(){
 
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] Last known agglayer certificate height: $height, status: $status" >&3
 
-    if (( height > settle_certificates_target - 1 )); then
+    if ((height > settle_certificates_target - 1)); then
         echo "[$(date '+%Y-%m-%d %H:%M:%S')] ✅ Success! The number of settled certificates has reached the target." >&3
         exit 0
     fi
 
-    if (( height == settle_certificates_target - 1 )); then
+    if ((height == settle_certificates_target - 1)); then
         if [ "$status" == "Settled" ]; then
             echo "[$(date '+%Y-%m-%d %H:%M:%S')] ✅ Success! The number of settled certificates has reached the target." >&3
             exit 0
         fi
-        
+
         echo "[$(date '+%Y-%m-%d %H:%M:%S')] ⚠️ Warning! The number of settled certificates is one less than the target." >&3
     fi
 }
@@ -73,7 +73,7 @@ function extract_certificate_height() {
 
 function extract_certificate_status() {
     local cast_output="$1"
-     echo "$cast_output" | jq -r '.status'
+    echo "$cast_output" | jq -r '.status'
 }
 
 # MAIN
