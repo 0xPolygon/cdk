526424adc649b995a04a039aade5867332f3b4af
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2025-04-04 15:58:35 +0200

    fix: coverage

--- sequencesender/sequencesender_test.go
+++ sequencesender/sequencesender_test.go
@@ -624,5 +624,6 @@ func Test_isEthTxManagerErrNotFound(t *testing.T) {
 	require.False(t, isEthTxManagerErrNotFound(nil))
 	require.True(t, isEthTxManagerErrNotFound(ethtxmanager.ErrNotFound))
 	require.True(t, isEthTxManagerErrNotFound(fmt.Errorf("is wrapped %w", ethtxmanager.ErrNotFound)))
+	require.False(t, isEthTxManagerErrNotFound(fmt.Errorf("another error")))
 
 }
