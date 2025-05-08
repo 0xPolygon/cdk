7f4ec6751217f9513732f08b11880215efc46a28
Arpit Temani <temaniarpit27@gmail.com>
2025-05-08 13:10:39 +0530

    review comments fix

--- cmd/run.go
+++ cmd/run.go
@@ -386,7 +386,7 @@ func runL1InfoTreeSyncerIfNeeded(
 		cfg.L1InfoTreeSync.MaxRetryAttemptsAfterError,
 		l1infotreesync.FlagNone,
 		aggkitetherman.FinalizedBlock,
-		false,
+		true,
 	)
 	if err != nil {
 		log.Fatal(err)
