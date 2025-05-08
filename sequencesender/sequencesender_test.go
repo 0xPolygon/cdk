7abad80b94af2b83de43b514b3dca4b5e3a0542d
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2025-05-08 11:31:23 +0200

    fix: increase coverage

--- sequencesender/sequencesender_test.go
+++ sequencesender/sequencesender_test.go
@@ -1,6 +1,7 @@
 package sequencesender
 
 import (
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"math/big"
@@ -26,9 +27,10 @@ import (
 )
 
 const (
-	txStreamEncoded1 = "f86508843b9aca0082520894617b3a3528f9cdd6630fd3301b9c8911f7bf063d0a808207f5a0579b72a1c1ffdd845fba45317540982109298e2ec8d67ddf2cdaf22e80903677a01831e9a01291c7ea246742a5b5a543ca6938bfc3f6958c22be06fad99274e4ac"
-	txStreamEncoded2 = "f86509843b9aca0082520894617b3a3528f9cdd6630fd3301b9c8911f7bf063d0a808207f5a0908a522075e09485166ffa7630cd2b7013897fa1f1238013677d6f0a86efb3d2a0068b12435fcdc8ee254f3b1df8c5b29ed691eeee6065704f061130935976ca99"
-	txStreamEncoded3 = "b8b402f8b101268505d21dba0085076c363d8982dc60941929761e87667283f087ea9ab8370c174681b4e980b844095ea7b300000000000000000000000080a64c6d7f12c47b7c66c5b4e20e72bc1fcd5d9effffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc001a0dd4db494969139a120e8721842455ec13f82757a4fc49b66d447c7d32d095a1da06ef54068a9aa67ecc4f52d885299a04feb6f3531cdfc771f1412cd3331d1ba4c"
+	txStreamEncoded1   = "f86508843b9aca0082520894617b3a3528f9cdd6630fd3301b9c8911f7bf063d0a808207f5a0579b72a1c1ffdd845fba45317540982109298e2ec8d67ddf2cdaf22e80903677a01831e9a01291c7ea246742a5b5a543ca6938bfc3f6958c22be06fad99274e4ac"
+	txStreamEncoded2   = "f86509843b9aca0082520894617b3a3528f9cdd6630fd3301b9c8911f7bf063d0a808207f5a0908a522075e09485166ffa7630cd2b7013897fa1f1238013677d6f0a86efb3d2a0068b12435fcdc8ee254f3b1df8c5b29ed691eeee6065704f061130935976ca99"
+	txStreamEncoded3   = "b8b402f8b101268505d21dba0085076c363d8982dc60941929761e87667283f087ea9ab8370c174681b4e980b844095ea7b300000000000000000000000080a64c6d7f12c47b7c66c5b4e20e72bc1fcd5d9effffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc001a0dd4db494969139a120e8721842455ec13f82757a4fc49b66d447c7d32d095a1da06ef54068a9aa67ecc4f52d885299a04feb6f3531cdfc771f1412cd3331d1ba4c"
+	batchCodedRLP2Txs1 = "0b73e6af6f00000000ee02843b9aca00830186a0944d5cf5032b2a844602278b01199ed191a86c93ff88016345785d8a0000808203e88080bff0e780ba7db409339fd3f71969fa2cbf1b8535f6c725a1499d3318d3ef9c2b6340ddfab84add2c188f9efddb99771db1fe621c981846394ea4f035c85bcdd51bffee03843b9aca00830186a0944d5cf5032b2a844602278b01199ed191a86c93ff88016345785d8a0000808203e880805b346aa02230b22e62f73608de9ff39a162a6c24be9822209c770e3685b92d0756d5316ef954eefc58b068231ccea001fb7ac763ebe03afd009ad71cab36861e1bff"
 )
 
 var (
@@ -697,4 +699,21 @@ func Test_batchRetrieval(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_populateSequenceData(t *testing.T) {
+	sut := SequenceSender{
+		cfg: Config{
+			GetBatchWaitInterval: types2.NewDuration(time.Millisecond),
+		},
+		logger:       log.GetDefaultLogger(),
+		sequenceData: make(map[uint64]*sequenceData),
+	}
+	batchL2Data, err := hex.DecodeString(batchCodedRLP2Txs1)
+	require.NoError(t, err)
+	batch := rpctypes.NewRPCBatch(1, common.Hash{}, nil, batchL2Data,
+		common.Hash{}, common.Hash{}, common.Hash{}, common.Address{}, true)
+
+	err = sut.populateSequenceData(batch, 1234)
+	require.NoError(t, err)
 }
